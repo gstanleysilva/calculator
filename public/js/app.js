edc96a9b13f9d151eef780f6c064cc0a86d601c6
Gustavo Stanley <guhstanley@gmail.com>
2020-05-02 14:18:07 -0300

    Teste do git novamente

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ btn.addEventListener('click', () => {
 
     fetch(service).then((response) => {
         response.json().then((data) => {
-            console.log(data);
+            console.log(data)
         })
     })
 
